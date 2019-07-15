e84eafa0e7edb6c767d8e1874a53bfe3e3965804
raisulkutumbita <raisul@kutumbita.com>
2019-07-15 15:15:14 +0600

    Added skill

--- src/container/Skills.js
+++ src/container/Skills.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Tag } from "antd";
 import { TitleBlock } from "../components";
 
-const skill = ["Python", "JavaScript", "AWS", "Django", "Flask", "Node js", "Numpy", "Pandas", "Redis", "Design Pattern", "ELK", "Postgres", "Nginx", "Gunicorn", "Docker", "CI/CD"]
+const skill = ["Python", "JavaScript", "AWS", "Django", "Flask", "Node js", "Express JS", "Numpy", "Pandas", "Redis", "Design Pattern", "ELK", "Postgres", "Nginx", "Gunicorn", "Docker", "CI/CD"]
 
 function Skills() {
   return (
